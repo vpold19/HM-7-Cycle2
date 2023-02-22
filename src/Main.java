44b8d52bf94ad49c77f2bb3ad5af3b509cb9c2ed
vadim <vadimpoldolin19@gmail.com>
2023-02-22 21:46:00 +0300

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+    public static void task1(){
+        System.out.println("Задание 1");
+    }
+    public static void task2(){
+        System.out.println("Задание 2");
+    }
+    public static void task3(){
+        System.out.println("Задание 3");
+    }
+    public static void task4(){
+        System.out.println("Задание 4");
+    }
+    public static void task5(){
+        System.out.println("Задание 5");
+    }
+    public static void task6(){
+        System.out.println("Задание 6");
+    }
+    public static void task7(){
+        System.out.println("Задание 7");
+    }
+    public static void task8(){
+        System.out.println("Задание 8");
+    }
+}
